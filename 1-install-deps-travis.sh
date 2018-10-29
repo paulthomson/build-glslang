e91ec4d97076b70c2111279e1e997abfdf58dcaf
Paul Thomson <paulthomson@users.noreply.github.com>
2018-10-29 12:42:40 +0000

    Update scripts and commit id (#1)
    * Update build scripts.
    
    * Fix Mac release script and add .version files for libs/ directory.
    
    * Update to commit f0928b3f569c595737f812572b8279fdf0d27b47
    
    * Add -DCMAKE_OSX_ARCHITECTURES=x86_64

--- 1-install-deps-travis.sh
+++ 1-install-deps-travis.sh
@@ -6,13 +6,13 @@ set -u
 sudo mkdir -p /data/bin
 sudo chmod uga+rwx /data/bin
 
-GITHUB_RELEASE_TOOL_USER="paulthomson"
-GITHUB_RELEASE_TOOL_VERSION="v1.0.9.1"
+GITHUB_RELEASE_TOOL_USER="c4milo"
+GITHUB_RELEASE_TOOL_VERSION="v1.1.0"
 
 
 if [ "$(uname)" == "Darwin" ];
 then
-  brew install ninja
+  brew install ninja md5sha1sum
   GITHUB_RELEASE_TOOL_ARCH="darwin_amd64"
 fi
 
@@ -28,7 +28,7 @@ wget "https://github.com/${GITHUB_RELEASE_TOOL_USER}/github-release/releases/dow
 tar xf "github-release_${GITHUB_RELEASE_TOOL_VERSION}_${GITHUB_RELEASE_TOOL_ARCH}.tar.gz"
 popd
 
-git clone https://github.com/KhronosGroup/glslang.git
-cd glslang
+git clone https://github.com/KhronosGroup/glslang.git "${CLONE_DIR}"
+cd "${CLONE_DIR}"
 git checkout $(cat ../COMMIT_ID)
 
